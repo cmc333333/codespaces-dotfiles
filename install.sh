a4531df0c57b7c0590ba112ffe673233626ab18b
CM Lubinski <cmc333333@gmail.com>
2024-01-22 04:18:31 +0000

    Add a better installation location for nvim.

--- install.sh
+++ install.sh
@@ -2,7 +2,5 @@
 set -euo pipefail
 
 sudo apt-get update && sudo apt-get install -y fuse
-mkdir -p /usr/bin
-cd /usr/bin
-curl --location --output nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim
+sudo curl --location --output /usr/bin/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+sudo chmod a+x /usr/bin/nvim
