78292d99370c76c013296d50dabc14c25275d293
CM Lubinski <cmc333333@gmail.com>
2024-01-21 23:13:16 -0500

    Put nvim in a better path

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 sudo apt-get update && sudo apt-get install -y fuse
-mkdir -p ~/bin
-cd ~/bin
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
+mkdir -p /usr/bin
+cd /usr/bin
+curl --location --output nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim
