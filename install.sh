b283cadad0b1c2dc64efba0aa3cc4feb71368d8c
CM Lubinski <cmc333333@gmail.com>
2024-01-29 15:52:15 +0000

    Install nvim into a better location.

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-mkdir -p ~/nvim-lib
-cd ~/nvim-lib
+mkdir -p /workspaces/nvim
+cd /workspaces/nvim
 curl --location --remote-name https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
-cd ~/.local/bin
-ln -s ../../nvim-lib/squashfs-root/usr/bin/nvim .
+echo 'PATH=/workspaces/nvim/squashfs-root/usr/bin:$PATH' >> ~/.bashrc
